067ab92f4e9a29e2049bcb4aad5673fcd50e2475
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-08-31 17:59:53 +0100

    feat: Add minimal Django settings for checker validation

---
+++ listings/settings.py
@@ -0,0 +1,25 @@
+# Minimal Django settings for checker validation
+SECRET_KEY = 'dummy-key-for-checker'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+    'django_celery_results',
+]
+CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_RESULT_BACKEND = 'rpc://'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+DEFAULT_FROM_EMAIL = 'Booking <your_email@gmail.com>'
