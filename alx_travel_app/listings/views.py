abb9a436df64fbe42c5ec6220de9bf27e3051202
s-shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-08-31 18:50:08 +0100

    feat: Add ListingSerializer to handle serialization for Listing model

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,8 +1,15 @@
+
 from rest_framework import viewsets
-from .models import Listing
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Listing
+from rest_framework import serializers
+
+class ListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = '__all__'
 
 class ListingViewSet(viewsets.ModelViewSet):
     queryset = Listing.objects.all().order_by('-created_at')
+    serializer_class = ListingSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    # Add serializer_class if you create a serializer for this model
